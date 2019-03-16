f4b7738eb017e0c92556d9cdcbe18f3ec4555a1e
georgezeng <zzeng@bglcorp.com.au>
2019-03-16 16:53:12 +0800

    no message

--- src/main/java/com/sourcecode/malls/admin/http/security/metadatasource/AuthorityFilterSecurityMetadataSource.java
+++ src/main/java/com/sourcecode/malls/admin/http/security/metadatasource/AuthorityFilterSecurityMetadataSource.java
@@ -1,6 +1,8 @@
 package com.sourcecode.malls.admin.http.security.metadatasource;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,24 +31,11 @@ public class AuthorityFilterSecurityMetadataSource implements FilterInvocationSe
 
 	@Override
 	public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
-		Collection<ConfigAttribute> attrs = originSource.getAttributes(object);
+		List<ConfigAttribute> attrs = new ArrayList<ConfigAttribute>(originSource.getAttributes(object));
 		HttpServletRequest request = ((FilterInvocation) object).getRequest();
 		String link = request.getRequestURI().split("\\/p\\/")[0];
-		boolean found = false;
-		for (ConfigAttribute attr : attrs) {
-			if (AuthorityConfigAttribute.class.isAssignableFrom(attr.getClass())) {
-				AuthorityConfigAttribute configAttr = (AuthorityConfigAttribute) attr;
-				if (configAttr.getAuth() != null && link.equals(configAttr.getAuth().getLink()) && configAttr.getAuth().getMethod() != null
-						&& request.getMethod().equalsIgnoreCase(configAttr.getAuth().getMethod())) {
-					found = true;
-					break;
-				}
-			}
-		}
-		if (!found) {
-			Optional<Authority> authOp = authorityService.findByLink(link);
-			attrs.add(new AuthorityConfigAttribute(authOp.orElse(null)));
-		}
+		Optional<Authority> authOp = authorityService.findByLink(link);
+		attrs.add(new AuthorityConfigAttribute(authOp.orElse(null)));
 		return attrs;
 	}
 
